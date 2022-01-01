33df2a47c73e34cad1cb35f11480d843bde97352
Hedy <43907004+Hedy88@users.noreply.github.com>
2022-01-01 20:11:53 +0000

    a
     v

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+  // $MYSQL_SERVER = "";
+  // $MYSQL_USER = "";
+  // $MYSQL_PASSWORD = "";
+  
+  // $conn = new mysqli($MYSQL_SERVER, $MYSQL_USER, $MYSQL_PASS); 
+
+  // if ($conn->connect_error) {
+    //  die("MySQL Error: " . $conn->connect_error);
+  // }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+     <meta charset="UTF-8">
+     <title>jankentube - Home</title>
+     <link rel="stylesheet" href="./assets/css/main.css">
+    </head>
+    <body> 
+        <main> 
+          <div class="top">
+            <a href="/">
+              <h1>jankentube</h1>
+            </a>
+            <hr>
+            <div class="links">
+              <a href="/">Home</a>
+              <a href="/channels">Channels</a>
+            </div>
+            <hr>
+       </div>
+       <div class="content">
+           <div class="hdr-title">hot videos</div>
+       </div>
+       <main>
+    </body>
+</html>
